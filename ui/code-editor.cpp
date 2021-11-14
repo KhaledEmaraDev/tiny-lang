ce613d97bc999f6ee335f09f3fded0f331f2261f
Hosni Adel <hosni.adel666@gmail.com>
2021-11-14 22:49:26 +0200

    ui: Add CodeEditor

---
+++ ui/code-editor.cpp
@@ -0,0 +1,142 @@
+#include "code-editor.h"
+
+#include <QDebug>
+#include <QPainter>
+#include <QTextBlock>
+
+CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
+{
+    lineNumberArea = new LineNumberArea(this);
+
+    connect(this, &CodeEditor::blockCountChanged, this, &CodeEditor::updateLineNumberAreaWidth);
+    connect(this, &CodeEditor::updateRequest, this, &CodeEditor::updateLineNumberArea);
+    connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
+
+    updateLineNumberAreaWidth(0);
+    highlightCurrentLine();
+}
+
+void CodeEditor::displayError(int line, const QString &msg) {
+    QTextCursor errorCursor(document()->findBlockByLineNumber(line / 2));
+    errorCursor.movePosition(QTextCursor::EndOfLine);
+
+    QTextCharFormat errorFormat;
+    errorFormat.setFontItalic(true);
+    errorFormat.setFontWeight(QFont::ExtraBold);
+    errorFormat.setForeground(Qt::red);
+    errorCursor.setCharFormat(errorFormat);
+
+    int originalPosition = errorCursor.position();
+    errorCursor.insertText(" Error: " + msg);
+    errorCursor.setPosition(originalPosition);
+    errorCursor.movePosition(QTextCursor::EndOfLine, QTextCursor::KeepAnchor);
+
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly()) {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor(Qt::red).lighter(180);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = errorCursor;
+        errorSelections.append(selection);
+    }
+
+    extraSelections.append(currentLineSelection);
+    extraSelections.append(errorSelections);
+    setExtraSelections(extraSelections);
+}
+
+void CodeEditor::clearErrors() {
+    for (auto &selection: errorSelections) {
+        selection.cursor.removeSelectedText();
+    }
+
+    errorSelections.clear();
+}
+
+int CodeEditor::lineNumberAreaWidth()
+{
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+
+    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+    return space;
+}
+
+void CodeEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
+{
+    setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+}
+
+void CodeEditor::updateLineNumberArea(const QRect &rect, int dy)
+{
+    if (dy)
+        lineNumberArea->scroll(0, dy);
+    else
+        lineNumberArea->update(0, rect.y(), lineNumberArea->width(), rect.height());
+
+    if (rect.contains(viewport()->rect()))
+        updateLineNumberAreaWidth(0);
+}
+
+void CodeEditor::resizeEvent(QResizeEvent *e)
+{
+    QPlainTextEdit::resizeEvent(e);
+
+    QRect cr = contentsRect();
+    lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+}
+
+void CodeEditor::highlightCurrentLine()
+{
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly()) {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor(Qt::yellow).lighter(160);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = textCursor();
+        selection.cursor.clearSelection();
+        currentLineSelection = selection;
+    }
+
+    extraSelections.append(currentLineSelection);
+    extraSelections.append(errorSelections);
+    setExtraSelections(extraSelections);
+}
+
+void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
+{
+    QPainter painter(lineNumberArea);
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+    QTextBlock block = firstVisibleBlock();
+    int blockNumber = block.blockNumber();
+    int top = qRound(blockBoundingGeometry(block).translated(contentOffset()).top());
+    int bottom = top + qRound(blockBoundingRect(block).height());
+
+    while (block.isValid() && top <= event->rect().bottom()) {
+        if (block.isVisible() && bottom >= event->rect().top()) {
+            QString number = QString::number(blockNumber + 1);
+            painter.setPen(Qt::black);
+            painter.drawText(0, top, lineNumberArea->width(), fontMetrics().height(),
+                             Qt::AlignRight, number);
+        }
+
+        block = block.next();
+        top = bottom;
+        bottom = top + qRound(blockBoundingRect(block).height());
+        ++blockNumber;
+    }
+}
