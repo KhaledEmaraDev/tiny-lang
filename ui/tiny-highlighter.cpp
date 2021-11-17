fa70908a724209b8685da86e341b87672bf2c0d4
Ahmed <ahmedellord56@gmail.com>
2021-11-17 20:31:38 +0200

    ui: Add Syntax Highlighter

---
+++ ui/tiny-highlighter.cpp
@@ -0,0 +1,56 @@
+#include "tiny-highlighter.h"
+
+TinyHighlighter::TinyHighlighter(QTextDocument *parent)
+    : QSyntaxHighlighter(parent)
+{
+    HighlightingRule rule;
+
+    keywordFormat.setForeground(Qt::darkBlue);
+    keywordFormat.setFontWeight(QFont::Bold);
+    const QString keywordPatterns[] = {
+        QStringLiteral("\\bif\\b"), QStringLiteral("\\bthen\\b"), QStringLiteral("\\belse\\b"),
+        QStringLiteral("\\bend\\b"), QStringLiteral("\\brepeat\\b"), QStringLiteral("\\buntil\\b"),
+        QStringLiteral("\\bread\\b"), QStringLiteral("\\bwrite\\b")
+    };
+    for (const QString &pattern : keywordPatterns) {
+        rule.pattern = QRegularExpression(pattern);
+        rule.format = keywordFormat;
+        highlightingRules.append(rule);
+    }
+
+    multiLineCommentFormat.setForeground(Qt::red);
+
+    commentStartExpression = QRegularExpression(QStringLiteral("{"));
+    commentEndExpression = QRegularExpression(QStringLiteral("}"));
+}
+
+void TinyHighlighter::highlightBlock(const QString &text)
+{
+    for (const HighlightingRule &rule : qAsConst(highlightingRules)) {
+        QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
+        while (matchIterator.hasNext()) {
+            QRegularExpressionMatch match = matchIterator.next();
+            setFormat(match.capturedStart(), match.capturedLength(), rule.format);
+        }
+    }
+    setCurrentBlockState(0);
+
+    int startIndex = 0;
+    if (previousBlockState() != 1)
+        startIndex = text.indexOf(commentStartExpression);
+
+    while (startIndex >= 0) {
+        QRegularExpressionMatch match = commentEndExpression.match(text, startIndex);
+        int endIndex = match.capturedStart();
+        int commentLength = 0;
+        if (endIndex == -1) {
+            setCurrentBlockState(1);
+            commentLength = text.length() - startIndex;
+        } else {
+            commentLength = endIndex - startIndex
+                            + match.capturedLength();
+        }
+        setFormat(startIndex, commentLength, multiLineCommentFormat);
+        startIndex = text.indexOf(commentStartExpression, startIndex + commentLength);
+    }
+}
---
+++ ui/tiny-highlighter.h
@@ -0,0 +1,36 @@
+#ifndef TINYHIGHLIGHTER_H
+#define TINYHIGHLIGHTER_H
+
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+#include <QRegularExpression>
+
+QT_BEGIN_NAMESPACE
+class QTextDocument;
+QT_END_NAMESPACE
+
+class TinyHighlighter : public QSyntaxHighlighter
+{
+    Q_OBJECT
+public:
+    TinyHighlighter(QTextDocument *parent = 0);
+
+protected:
+    void highlightBlock(const QString &text) override;
+
+private:
+    struct HighlightingRule
+    {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+    };
+    QVector<HighlightingRule> highlightingRules;
+
+    QRegularExpression commentStartExpression;
+    QRegularExpression commentEndExpression;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat multiLineCommentFormat;
+};
+
+#endif // TINYHIGHLIGHTER_H
